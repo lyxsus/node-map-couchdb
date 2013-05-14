6a9c9df807e428433b7b9378098c365497665f41
Sergey Antoninko <lyxsus@gmail.com>
2013-05-14 16:08:21 +0400

    dev

--- mirror/futurios-sync-master/_attachments/config.js
+++ mirror/futurios-sync-master/_attachments/config.js
@@ -2,7 +2,8 @@ define ({
 	pool: {
 		server: {
 			// host: '192.168.1.42',
-			host: '89.179.119.16',
+			// host: '89.179.119.16',
+			host: '127.0.0.1',
 			auth: {
 				username: 'lyxsus@gmail.com',
 				password: 'letmein'
